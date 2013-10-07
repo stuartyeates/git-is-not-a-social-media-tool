835cc574bc0acc31f38862f7b936ecb4123354f9
stuart yeates <syeates@gmail.com>
2013-10-07 13:30:04 +1300

    generate-fake-history.sh

---
+++ generate-fake-history.sh
@@ -0,0 +1,16 @@
+#!\bin\bash
+#a script to re-create a file, one line at a time, one day per line
+
+
+
+
+for i in {0..1673}; 
+do 
+    echo $i;  
+    cat  nzetc.csv | rev | sort | rev | head --lines $i > nzetc-urls.csv; 
+    (( j = 1673 - $i )) ;
+    echo $j ; 
+    date -d "$j days ago"; 
+    echo GIT_AUTHOR_DATE=\"`date -d "$j days ago"`\" GIT_COMMITTER_DATE=\"`date -d "$j days ago"`\"  git commit -m 'add'
+
+done 
